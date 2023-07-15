00cb8f721a9fc89f1430fdeb580fec2d22d0f8b8
Νίκος Σούρλος <soyrlos@gmail.com>
2023-07-15 21:24:49 +0200

    'description_correction'

--- review_manuscript_streamlit.py
+++ review_manuscript_streamlit.py
@@ -36,9 +36,9 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("Have a manuscript to review? Takes a lot of time? This tool is meant to help you generate \
-                review questions for it. \n \
+                review questions for it. \n\n \
                 There is a limit of 16k tokens (with response). Takes only pdf files as input. \
-                \n\nThis tool is made  by [Nikos Sourlos](https://linkedin.com/in/nsourlos). Source Code on [Github](https://github.com/nsourlos/review_manuscript_streamlit/blob/main/review_manuscript_streamlit.py)")
+                \n\nThis tool is made  by [Nikos Sourlos](https://linkedin.com/in/nsourlos). \n\n Source Code on [Github](https://github.com/nsourlos/review_manuscript_streamlit/blob/main/review_manuscript_streamlit.py)")
 
 with col2:
     # st.image(image='paper_review.jpg', width=300, caption='https://www.nature.com/articles/d41586-018-06991-0')
